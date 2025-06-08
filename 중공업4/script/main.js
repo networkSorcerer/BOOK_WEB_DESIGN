7af69527e952a4e133b52e655a1a0f58b320603e
networdSorcerer <gigattfyhig@gmail.com>
2025-06-09 04:44:21 +0900

    웹디 과연 붙을 수 있을 것인가? 이제는 붙고 싶다

--- 중공업4/script/main.js
+++ 중공업4/script/main.js
@@ -1,15 +1,15 @@
-let mainMenu = $(".main_menu >li");
+let mainMenu = $('.main_menu >li');
 
 mainMenu
   .mouseover(function () {
-    $(this).find("ul").stop().slideDown();
+    $(this).find('ul').stop().slideDown();
   })
   .mouseout(function () {
-    $(this).find("ul").stop().slideUp();
+    $(this).find('ul').stop().slideUp();
   });
 
-let slideContainer = $(".slide_container");
-let slide = slideContainer.find(".slide");
+let slideContainer = $('.slide_container');
+let slide = slideContainer.find('.slide');
 let slideCount = slide.length;
 let currentIdx = 0;
 
@@ -23,27 +23,27 @@ function ShowNextSlide() {
   currentIdx = nextIdx;
 }
 
-let tabMenu = $(".tabmenu > li");
-let tabContent = $(".tabcontent > div");
+let tabMenu = $('.tabmenu > li');
+let tabContent = $('.tabcontent > div');
 
 tabMenu.click(function (e) {
   e.preventDefault;
-  tabMenu.removeClass("active");
-  $(this).addClass("active");
+  tabMenu.removeClass('active');
+  $(this).addClass('active');
 
-  let target = $(this).find("a").attr("href");
-  tabContent.removeClass("active");
-  $(target).addClass("active");
+  let target = $(this).find('a').attr('href');
+  tabContent.removeClass('active');
+  $(target).addClass('active');
 });
 
-let popupLink = $("#notice li:first");
-let popup = $("#popup");
-let popupCloseBtn = popup.find(".close");
+let popupLink = $('#notice li:first');
+let popup = $('#popup');
+let popupCloseBtn = popup.find('.close');
 
 popupLink.click(function (e) {
   e.preventDefault;
-  popup.addClass("active");
+  popup.addClass('active');
 });
 popupCloseBtn.click(function () {
-  popup.removeClass("active");
+  popup.removeClass('active');
 });
