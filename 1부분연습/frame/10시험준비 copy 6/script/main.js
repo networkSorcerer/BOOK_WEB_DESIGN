80a38f82554a50a2b662ab1fa6a62f6c851f9ce3
networkSocerer <gigattfyhig@gmail.com>
2025-09-03 15:18:39 +0900

    웹디 합격하자~~~git add .git add .!

---
+++ 1부분연습/frame/10시험준비 copy 6/script/main.js
@@ -0,0 +1,51 @@
+let mainMenu = $("nav> ul >li");
+let subMenu = mainMenu.find("ul");
+
+mainMenu
+  .mouseover(function () {
+    subMenu.stop().slideDown();
+  })
+  .mouseout(function () {
+    subMenu.stop().slideUp();
+  });
+
+// let slideContainer = $(".container");
+// let slide = slideContainer.find(".slide");
+// let slideCount = slide.length;
+// let currentIdx = 0;
+
+// function autoSlide() {
+//   setInterval(function () {
+//     let nextIdx = (currentIdx + 1) % slideCount;
+//     slideContainer.animate({ marginTop: -300 * nextIdx + "px" });
+//     currentIdx = nextIdx;
+//   }, 3000);
+// }
+// autoSlide();
+// let slideContainer = $(".container");
+// let slide = slideContainer.find(".slide");
+// let slideCount = slide.length;
+// let currentIdx = 0;
+
+// function autoSlide() {
+//   setInterval(function () {
+//     let nextIdx = (currentIdx + 1) % slideCount;
+//     slideContainer.animate({ marginTop: -300 * nextIdx + "px" });
+//     currentIdx = nextIdx;
+//   }, 3000);
+// }
+// autoSlide();
+
+let slideContainer = $(".container");
+let slide = slideContainer.find(".slide");
+let slideCount = slide.length;
+let currentIdx = 0;
+
+function autoSlide() {
+  setInterval(function () {
+    let nextIdx = (currentIdx + 1) % slideCount;
+    slideContainer.animate({ marginTop: -300 * nextIdx + "px" });
+    currentIdx = nextIdx;
+  }, 3000);
+}
+autoSlide();
