3c19bf175599ce07ff4c105a9e99fedcd6bfa57f
networkSocerer <gigattfyhig@gmail.com>
2025-09-04 17:12:45 +0900

    update

---
+++ 1부분연습/footer/10시험준비 copy 3/script/main.js
@@ -0,0 +1,10 @@
+let popupLink = $("#notice li:first");
+let popup = $("#popup");
+let popupCloseBtn = popup.find(".close");
+popupLink.click(function (e) {
+  e.preventDefault();
+  popup.addClass("active");
+});
+popupCloseBtn.click(function () {
+  popup.removeClass("active");
+});
